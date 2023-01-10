0c66338403c7c8023846a69aaf43a44e668939b0
Andy <s1810942008@gms.nutc.edu.tw>
2023-01-10 19:32:50 +0800

    Actor movement prototype

---
+++ Assets/Game/System/ActorMovement/Prototype/Scripts/ActorMovementPrototype_sc.cs
@@ -0,0 +1,27 @@
+﻿using AutoBot.Utilities.Extensions;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ActorMovement.Prototype.Scripts
+{
+    public class ActorMovementPrototype_sc : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text textState;
+        
+
+        [SerializeField] private Button buttonMove;
+
+        void Start()
+        {
+            textState.text = "idle";
+            buttonMove.BindClick(MoveToward);
+        }
+
+        private void MoveToward()
+        {
+            transform.position += Vector3.right;
+            textState.text = "Moving";
+        }
+    }
+}
